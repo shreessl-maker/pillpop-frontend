7974b812544ed723b8c640f0bd289cdd7902c8e3
shreessl-maker <shreessl@gmail.com>
2025-12-09 13:40:18 +0530

    Create DashboardStaff.jsx

---
+++ src/pages/DashboardStaff.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function DashboardStaff() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#f0f9ff",
+      fontSize: "24px",
+      color: "#1d4ed8"
+    }}>
+      <div>
+        <h1>PharmIQ Staff Dashboard</h1>
+        <p>This is a placeholder dashboard for staff. Functionality will be added soon!</p>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardStaff;
