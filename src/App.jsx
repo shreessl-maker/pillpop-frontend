81914c1eb9d5cc854151bd3e3096289b8a0bb925
shreessl-maker <shreessl@gmail.com>
2025-12-09 13:56:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import DashboardSuperAdmin from "./pages/DashboardSuperAdmin.jsx";
-import DashboardAdmin from "./pages/DashboardAdmin.jsx";
-import DashboardStaff from "./pages/DashboardStaff.jsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import DashboardSuperAdmin from "./pages/DashboardSuperAdmin";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/superadmin/dashboard" element={<DashboardSuperAdmin />} />
-        <Route path="/admin/dashboard" element={<DashboardAdmin />} />
-        <Route path="/staff/dashboard" element={<DashboardStaff />} />
-        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
