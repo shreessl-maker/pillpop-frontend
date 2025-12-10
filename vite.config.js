1e1324a2d5f322b65c75baeb79d0cb21e6ff7193
shreessl-maker <shreessl@gmail.com>
2025-12-10 16:34:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ This line is critical for Vercel hosting
 export default defineConfig({
   plugins: [react()],
   base: "./",
