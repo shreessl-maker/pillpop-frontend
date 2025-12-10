7cf27c3814c620b9142685816aec6e6135e84dc5
shreessl-maker <shreessl@gmail.com>
2025-12-10 18:04:35 +0530

    Create Loader.jsx

---
+++ components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-white/80">
+      <div className="w-12 h-12 border-4 border-t-primary border-gray-300 rounded-full animate-spin"></div>
+    </div>
+  );
+}
