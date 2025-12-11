aec2c01f1efe4b48fde77f39b4140ac6509b759d
shreessl-maker <shreessl@gmail.com>
2025-12-11 08:14:34 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gradient-to-br from-blue-100 via-purple-100 to-white">
+      <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+    </div>
+  );
+}
 
