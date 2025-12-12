7aff72313cd03148df90c0671de814735a6719a3
shreessl-maker <shreessl@gmail.com>
2025-12-12 18:17:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// ✅ Create the root element and render the App
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
